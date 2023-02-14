eb59156411dcd5cede3f8ed1f90f57c30bfb5894
fanxiaohao <fanxiaohao@mail.taiji.com.cn>
2023-02-14 17:35:26 +0800

    数据劫持

--- ordinary/Observer.js
+++ ordinary/Observer.js
@@ -7,6 +7,29 @@ class Observer {
     if (!data || typeof data !== 'object') {
       return;
     }
+    Object.keys(data).forEach((key) => {
+      // 数据劫持
+      this.defineReactive(data, key, data[key]);
+      // 监听内部的值
+      this.observe(data[key]);
+    });
     console.log(data);
+  }
+
+  defineReactive(obj, key, value) {
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
+      // 控制台内可以看到被劫持的getter和setter
+      get() {
+        return value;
+      },
+      set(newValue) {
+        // 值改变才设置
+        if (newValue !== value) {
+          value = newValue;
+        }
+      }
+    });
   }
 }
